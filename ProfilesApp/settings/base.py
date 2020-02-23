dd7e771d5a1feddbaf80368216ffb890790ab480
Belkahla Mohamed Rachid <48124110+BisasterBoy32@users.noreply.github.com>
2020-02-24 00:56:25 +0100

    c1

--- ProfilesApp/settings/base.py
+++ ProfilesApp/settings/base.py
@@ -143,8 +143,8 @@ EMAIL_BACKEND ="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')  # "momomiko22@gmail.com"
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')  # "ihfytvseghcxesvr"
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')  
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')  
 
 # ---------------------- RESET PASSWORD SETTINGS --------------------
 DJANGO_REST_MULTITOKENAUTH_RESET_TOKEN_EXPIRY_TIME = 1
